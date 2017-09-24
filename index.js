c4b36a414d46c0ae949a039c26eafe5d3f107460
sumin <suminc7@gmail.com>
2017-09-25 00:39:37 +0900

    hostname test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var DEFAULT_PORT = 8080;
 var PORT = process.env.PORT || DEFAULT_PORT;
 
 app.get('/', function (req, res) {
-    res.send('Hello World~  ' + os.hostname())
+    res.send('Hello World~!  ' + os.hostname())
 })
 
 app.listen(PORT, function () {
